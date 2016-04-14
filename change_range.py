65ee6100fe5b13e974d18222c77d5999e8e8ec42
Alok Hota <ahota@vols.utk.edu>
2016-04-14 18:58:41 -0400

    fixed bug with float()

--- change_range.py
+++ change_range.py
@@ -1,12 +1,12 @@
 #simple one-liner
 def change_range_minimal(value, old_min, old_max, new_min, new_max):
-    return float((new_max-new_min)*(value-old_min)/(old_max-old_min)+new_min)
+    return (new_max-new_min)*float(value-old_min)/(old_max-old_min)+new_min
 
 #in case range is needed
 def change_range(value, old_min, old_max, new_min, new_max):
     old_range = old_max - old_min
     new_range = new_max - new_min
-    return float(new_range * (value - old_min)/old_range + new_min)
+    return new_range * float(value - old_min)/old_range + new_min
 
 if __name__ == '__main__':
     old_min = -5
